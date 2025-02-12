6fa47fe076e7cb809c0f39c41334a350cbf46ecf
yasaswini102 <yashaswini.vempalli@gmail.com>
2025-02-12 21:34:23 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,116 @@
+let name="kurnool"
+console.log(name.charAt(0))
+console.log(name.charAt(1))
+console.log(name.charAt(2))
+console.log(name.charAt(3))
+console.log(name.charAt(4))
+console.log(name.charAt(5))
+console.log(name.charAt(6))
+console.log(name.charAt(7))
+
+
+let nameB ="KODUR"
+console.log(nameB.toLocaleUpperCase())
+
+ let nameC ="KODUR"
+ console.log(nameC.toLocaleUpperCase())
+
+
+ let nameD ="KODUR"
+console.log(nameD.slice(0,7))
+
+
+let names ="tirupathi"
+let SplitName = names.slice(0,9)
+let Full_Name= "Random"
+console.log(names.slice(0,9))
+
+
+
+let name11 ="tirupathi"
+let SplitName11 = name11.slice(0,9)
+let SplitName22 = name11.substring(0,9)
+console.table([SplitName11,SplitName22])
+
+
+
+let name1 ="tirupathi"
+let SplitName1 = name1.slice(0,9)
+let SplitName2 = name1.substring(0,9)
+const newStringOne="                    ghytrsdfghjbc                    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+
+let url = "https://www.youtube.com/results?search_query=html%20js"
+console.log(url)
+console.log(`\n`)
+console.log(url.replace('%20','+'))
+
+
+
+let url1 = "https://www.youtube.com/results?search_\nquery=html%20js"
+console.log(url1)
+
+
+let url2 = "https://www.youtube.com/results?search_\tquery=html%20js"
+console.log(url2)
+
+
+
+let firstName="john"
+let lastName="alex"
+console.log(`hiii ${firstName} u are selected for xyz role in xyz company`)
+
+
+
+ let url3 = "https://www.youtube.com/results?search_query=html%20js"
+ console.log(url3.includes('youtube'))
+ console.log(url3.split('_'));
+
+
+
+let string1 = "ram"
+let string2 = "Mohan"
+let sumOfString1String2 = string1 + string2
+ console.log(sumOfString1String2)
+
+
+
+let array =[1,2,3,4,{name:"temp"}]
+console.log(array)
+
+
+
+let Year =[124,324,453,"yat","archu","yashu"]
+let Year1 =[
+    {
+    name:"yat",
+    rollNo:"124"
+    },
+    {
+        name:"archu",
+        rollNo:"324"
+    },
+    {
+        name:"yashu",
+        rollNo:"324"
+    },
+]
+console.log(Year)
+console.log(Year1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
