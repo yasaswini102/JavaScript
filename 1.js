dfd76465406a2d162cbd657c09397dd26100d35f
yasaswini102 <yashaswini.vempalli@gmail.com>
2025-02-12 21:33:02 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,39 @@
+// let num1 =1
+// let num2 ='1'
+// console.log(num1 == num2)
+
+// let num1 =1
+// let num2 ='1'
+// console.log(num1 === num2)
+
+
+
+let name="YAT"
+let name1 = name
+name1 = "loh"
+console.log(name1)
+console.log(name)
+
+
+let car =[
+    {
+        Name:"yashu",
+        RollNo:"12345",
+    },
+    {
+        Name:"thanu",
+        RollNo:"2345",  
+    },
+]
+let car1 = car
+car1[1].Name = "archu"
+console.log(car1)
+console.log(car)
+
+
+
+
+
+
+
+
---
+++ array.js
@@ -0,0 +1,60 @@
+ let arr1 =[3,4,5]
+  console.log(arr1)
+  arr1.push(7)
+  console.log(arr1)
+  arr1.pop()
+ console.log(arr1)
+
+
+
+
+let arr2 =[3,4,5]
+console.log(arr2)
+arr2.unshift(78)
+console.log(arr2)
+
+
+
+
+
+let arr3 =[3,4,5]
+console.log(arr)
+arr3.shift()
+console.log(arr)
+
+
+
+
+
+let arr4 =[3,4,5]
+console.log(arr4.indexof(5))
+console.log(arr4)
+
+
+
+
+let arr =[2,5,7];
+let newarr = arr.join()
+console.log(newarr)
+
+
+
+
+
+
+// new program//
+
+
+const myArr = [0,1,2,3,4,5]
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1, 3)
+console.log(myn1);
+
+console.log("B", myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log("C", myArr);
+console.log(myn2);
+
+
