03b27d6d1c66caa5663867a026b15ae0393556d3
yasaswini102 <yashaswini.vempalli@gmail.com>
2025-02-12 21:35:06 +0530

    Add files via upload

---
+++ datatype.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+// alert( 3 + 3) // we are using nodejs, not browser
+console.log(3 
+    +
+     3) // code readability should be high
+console.log("Ameer")
+let name = "Ameer"
+let age = 18
+let isLoggedIn = false
+let state;
+// number => The minimum value of integer is: -9007199254740991 The maximum value of integer is: 9007199254740991
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// object
+console.log(typeof undefined); // undefined
+console.log(typeof null); 
+
+
+
+
+
+
+let num1 =1
+  let num2 ='1123yat'
+  console.log(Number(num2))
+   console.log(typeof(String(num1)))
+   console.log(Boolean(num2))
+console.log(Object(num2))
